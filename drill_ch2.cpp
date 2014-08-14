4599cf0f7919cd411e2bfe24c787feed703b1b66
chomado <chomado@gmail.com>
2014-08-14 18:17:19 +0900

    answered questions after reading the chapter

---
+++ drill_ch2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+// #include "std_lib_facilities.h"
+using namespace std;
+inline void keep_window_open() {char ch; cin >> ch; }
+int main()
+{
+	cout << "Hello, World!\n";
+	keep_window_open(); // 1文字が入力されるまで待機
+	return 0;
+}
